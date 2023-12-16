20370803b4814c213288231658ec750e919c9447
Kevin Boschetti <kboschetti90@gmail.com>
2023-12-16 10:14:26 +0100

    delete

--- marketplace/item/urls.py
+++ marketplace/item/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'item'
 
 urlpatterns = [
+    path('new/', views.new, name='new'),
     # si aspetta un integer come private key, e sarà lo stesso espresso nella richiesta in item/view.py
     path('<int:pk>/', views.detail, name='detail'),
-    path('new/', views.new, name='new')
+    path('<int:pk>/delete//', views.delete, name='delete'),
 ]
